18efc12727de076abdbd7a0b1272220f0b9a2d9f
Gabriel <gcostaapps@gmail.com>
2022-05-10 22:01:11 -0300

    Added material in custom dialog

--- design_system/lib/src/dialogs/custom_dialog.dart
+++ design_system/lib/src/dialogs/custom_dialog.dart
@@ -20,87 +20,89 @@ class CustomDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      key: const ValueKey('CustomDialog'),
-      width: MediaQuery.of(context).size.width - 32,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: context.colorScheme.surface,
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            SpacerHeight24,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                title.toUpperCase(),
-                textAlign: TextAlign.center,
-                style: context.textTheme.subtitle2!.copyWith(
-                  fontWeight: FontWeight.w500,
-                  color: isWarning
-                      ? context.colorScheme.error
-                      : context.colorScheme.primary,
+    return Material(
+      child: SizedBox(
+        key: const ValueKey('CustomDialog'),
+        width: MediaQuery.of(context).size.width - 32,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: context.colorScheme.surface,
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SpacerHeight24,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  title.toUpperCase(),
+                  textAlign: TextAlign.center,
+                  style: context.textTheme.subtitle2!.copyWith(
+                    fontWeight: FontWeight.w500,
+                    color: isWarning
+                        ? context.colorScheme.error
+                        : context.colorScheme.primary,
+                  ),
                 ),
               ),
-            ),
-            SpacerHeight16,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                message,
-                textAlign: TextAlign.center,
+              SpacerHeight16,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  message,
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            SpacerHeight8,
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Material(
-                    type: MaterialType.transparency,
-                    child: InkWell(
-                      onTap: () => Navigator.pop(context),
-                      borderRadius: const BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        child: Icon(
-                          Icons.close,
-                          color: context.colorScheme.onBackground,
+              SpacerHeight8,
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: InkWell(
+                        onTap: () => Navigator.pop(context),
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: Icon(
+                            Icons.close,
+                            color: context.colorScheme.onBackground,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Material(
-                    type: MaterialType.transparency,
-                    child: InkWell(
-                      key: const ValueKey('CustomDialogConfirmButton'),
-                      onTap: () => onConfirm(context),
-                      borderRadius: const BorderRadius.only(
-                        bottomRight: Radius.circular(20),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        child: Icon(
-                          isWarning
-                              ? Icons.delete_outline_rounded
-                              : confirmIcon,
-                          color: isWarning
-                              ? context.colorScheme.error
-                              : context.colorScheme.primary,
+                  Expanded(
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: InkWell(
+                        key: const ValueKey('CustomDialogConfirmButton'),
+                        onTap: () => onConfirm(context),
+                        borderRadius: const BorderRadius.only(
+                          bottomRight: Radius.circular(20),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          child: Icon(
+                            isWarning
+                                ? Icons.delete_outline_rounded
+                                : confirmIcon,
+                            color: isWarning
+                                ? context.colorScheme.error
+                                : context.colorScheme.primary,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
