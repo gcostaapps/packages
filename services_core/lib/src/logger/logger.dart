6a67bab942886e3718697de4e61cb259d05e0d77
Gabriel Costa <gcostaapps@gmail.com>
2023-12-25 22:00:02 -0300

    Update logger

--- services_core/lib/src/logger/logger.dart
+++ services_core/lib/src/logger/logger.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: unused_local_variable, prefer_typing_uninitialized_variables
 
 import 'package:flutter/foundation.dart';
-import 'package:logger/logger.dart' as log;
 import 'package:logger/logger.dart';
+
 import 'i_logger.dart';
 
 class ProductionFilter extends LogFilter {
@@ -12,16 +12,16 @@ class ProductionFilter extends LogFilter {
   }
 }
 
-class Logger implements ILogger {
-  late final _logger;
+class AppLogger implements ILogger {
+  late Logger _logger;
 
-  Logger(this.releaseExceptionCatcher) {
+  AppLogger(this.releaseExceptionCatcher) {
     var release = true;
     assert(() {
       release = false;
       return true;
     }());
-    _logger = log.Logger(filter: ProductionFilter());
+    _logger = Logger(filter: ProductionFilter());
   }
 
   final List<String> messages = [];
@@ -31,12 +31,14 @@ class Logger implements ILogger {
 
   @override
   void debug(dynamic message, {dynamic error, StackTrace? stackTrace}) =>
-      kDebugMode ? _logger.d(message, error, stackTrace) : null;
+      kDebugMode
+          ? _logger.d(message, error: error, stackTrace: stackTrace)
+          : null;
 
   @override
   void error(dynamic message, {dynamic error, StackTrace? stackTrace}) {
     if (kDebugMode) {
-      _logger.e(message, error, stackTrace);
+      _logger.e(message, error: error, stackTrace: stackTrace);
     } else if (kReleaseMode && releaseExceptionCatcher != null) {
       releaseExceptionCatcher!(
         error,
@@ -48,11 +50,15 @@ class Logger implements ILogger {
 
   @override
   void info(dynamic message, {dynamic error, StackTrace? stackTrace}) =>
-      kDebugMode ? _logger.i(message, error, stackTrace) : null;
+      kDebugMode
+          ? _logger.i(message, error: error, stackTrace: stackTrace)
+          : null;
 
   @override
   void warning(dynamic message, {dynamic error, StackTrace? stackTrace}) =>
-      kDebugMode ? _logger.w(message, error, stackTrace) : null;
+      kDebugMode
+          ? _logger.w(message, error: error, stackTrace: stackTrace)
+          : null;
 
   @override
   void append(String message) => messages.add(message);
