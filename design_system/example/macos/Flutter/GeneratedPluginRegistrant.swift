5fa807f0468e6fca7d2f3addd56297582a0cea3e
Gabriel Costa <gcostaapps@gmail.com>
2025-06-03 12:10:43 -0300

    Removed reference to services core in DS

--- design_system/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ design_system/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_analytics
-import firebase_core
-import purchases_flutter
-import shared_preferences_foundation
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
