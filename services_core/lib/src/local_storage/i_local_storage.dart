d1d58bd289b277ea9607628ebaf0e42fbd87687f
Gabriel Costa <gcostaapps@gmail.com>
2025-06-03 12:00:43 -0300

    Removed services core

--- services_core/lib/src/local_storage/i_local_storage.dart
+++
@@ -1,7 +0,0 @@
-abstract class ILocalStorage {
-  Future<T?> read<T>(String key);
-  Future<void> write<T>(String key, T value);
-  Future<bool> contains(String key);
-  Future<void> remove(String key);
-  Future<void> clear();
-}
